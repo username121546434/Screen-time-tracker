b420a93d1728dad5f527f766ed6b9fb6f3a2f6cf
username121546434 <gokuleatharv06@gmail.com>
2024-06-08 19:20:58 -0400

    Added time period feature to pie chart

--- src/ui/pie_chart.py
+++ src/ui/pie_chart.py
@@ -1,5 +1,6 @@
 import csv
-from constants import APP_EXE_IDX, APP_NAME_IDX, FILE, TimePeriod
+from datetime import datetime
+from constants import APP_EXE_IDX, APP_NAME_IDX, DATE_FMT, FILE, TimePeriod
 from PySide6.QtGui import QPainter
 from PySide6.QtCharts import QChart, QChartView, QPieSeries, QPieSlice
 from PySide6.QtCore import QDate
@@ -21,19 +22,56 @@ class ScreenTimeChart(QPieSeries):
         self.chart_view = QChartView(self._chart)
         self.chart_view.setRenderHint(QPainter.Antialiasing)
     
-    def update(self, date: QDate, time_period: TimePeriod):
+    def update(self, date: QDate, time: TimePeriod):
         self.clear()
         lst = []
         total = 0
         with open(self.csv_source) as f:
             reader = csv.reader(f)
-            first_line = next(reader)
+            first_line = {}
+            for idx, i in enumerate(next(reader)):
+                try:
+                    first_line[datetime.strptime(i, DATE_FMT)] = idx
+                except ValueError:
+                    pass
 
             for row, line in enumerate(reader):
                 app_name = line[APP_NAME_IDX]
                 if app_name == 'None':
                     app_name = line[APP_EXE_IDX]
-                usage = sum(map(int, line[APP_EXE_IDX + 1:]))
+                
+                if time == 'All Time':
+                    usage = sum(map(int, line[APP_EXE_IDX + 1:]))
+                elif time == 'Day':
+                    day = date.toPython() # this returns a datetime.date object
+                    day = datetime(day.year, day.month, day.day)
+                    idx = first_line[day]
+                    usage = int(line[idx])
+                elif time == 'Month':
+                    month = date.month()
+                    year = datetime.now().year
+                    usage = 0
+                    for day in range(1, 32):
+                        try:
+                            day = datetime(year, month, day)
+                            idx = first_line[day]
+                        except (KeyError, ValueError):
+                            pass
+                        else:
+                            usage += int(line[idx])
+                elif time == 'Year':
+                    year = date.year()
+                    usage = 0
+                    for month in range(1, 13):
+                        for day in range(1, 32):
+                            try:
+                                day = datetime(year, month, day)
+                                idx = first_line[day]
+                            except (KeyError, ValueError):
+                                pass
+                            else:
+                                usage += int(line[idx])
+
                 total += usage
 
                 lst.append((app_name, usage))
