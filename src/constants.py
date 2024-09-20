e368b3c0c2d7b923d700144a556328004d7db58d
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2024-09-20 17:25:05 -0400

    Deleted the temp constants.py duplicate and copied and pasted it into /src/

--- src/constants.py
+++ src/constants.py
@@ -2,8 +2,14 @@ from pathlib import Path
 from typing import Literal
 
 DATE_FMT = r'%d-%m-%Y'
-FILE = Path('data.csv').resolve()
-APP_EXE_IDX = 1
-APP_NAME_IDX = 0
+DATE_FMT_SQL = r'date_%d_%m_%Y'
+FILE = Path('data.db').resolve()
+
+TABLE_NAME = 'Data'
+APP_NAME_COL = 'AppName'
+APP_EXE_COL = 'AppExe'
+
+APP_NAME_IDX = 1
+APP_EXE_IDX = 0
 
 TimePeriod = Literal['Day', 'Month', 'Year', 'All Time']
