6e8395a1d9cc4dfb3e2ef5b45cb56c962ee9be98
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2024-09-20 17:24:19 -0400

    Made tracker not use global variables

--- src/tracker/tracker.py
+++ src/tracker/tracker.py
@@ -1,32 +1,20 @@
-from get_process import get_active_window_app_name, get_active_window_app_path, get_app_name_from_exe
+from .get_process import get_active_window_app_name, get_active_window_app_path, get_app_name_from_exe
 import time
 from datetime import datetime
 from constants import FILE, DATE_FMT_SQL, TABLE_NAME, APP_EXE_COL, APP_NAME_COL
 import sqlite3
 
-database = sqlite3.connect(FILE)
-cursor = database.cursor()
-cursor.execute(f"""
-CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
-    {APP_EXE_COL} text,
-    {APP_NAME_COL} text,
-    {datetime.now().strftime(DATE_FMT_SQL)} INT default 0,
-    PRIMARY KEY ({APP_EXE_COL}, {APP_NAME_COL})
-);
-""")
-updates = 0
 
-
-def add_program(name: str, exe: str):
+def add_program(cursor: sqlite3.Cursor, name: str, exe: str):
     cursor.execute(f"""
 INSERT INTO {TABLE_NAME} ({APP_NAME_COL}, {APP_EXE_COL}, {datetime.now().strftime(DATE_FMT_SQL)})
 VALUES (?,?,?)
 """, (name, exe, 1))
-    database.commit()
+    cursor.connection.commit()
     return cursor.lastrowid
 
 
-def program_exists(name: str, exe: str):
+def program_exists(cursor: sqlite3.Cursor, name: str, exe: str):
     cursor.execute(f"""
 SELECT count(*)
 FROM {TABLE_NAME}
@@ -36,17 +24,17 @@ WHERE (({APP_NAME_COL} = ?) AND ({APP_EXE_COL} = ?))
     return data[0] != 0
 
 
-def increment_program(name: str, exe: str):
+def increment_program(cursor: sqlite3.Cursor, name: str, exe: str):
     column = datetime.now().strftime(DATE_FMT_SQL)
     cursor.execute(f"""
 UPDATE {TABLE_NAME}
 SET {column} = {column} + 1
 WHERE ({APP_NAME_COL} = ?) AND ({APP_EXE_COL} = ?)
 """, (name, exe))
-    database.commit()
+    cursor.connection.commit()
 
 
-def write_data(proc_name: str | None, app_name: str | None):
+def write_data(cursor: sqlite3.Cursor, proc_name: str | None, app_name: str | None):
     if proc_name is None or app_name == '':
         return
     try:
@@ -56,18 +44,18 @@ def write_data(proc_name: str | None, app_name: str | None):
     except sqlite3.OperationalError as e:
         print(e)
 
-    if not program_exists(app_name, proc_name):
+    if not program_exists(cursor, app_name, proc_name):
         print("New program added", app_name)
-        add_program(app_name, proc_name)
+        add_program(cursor, app_name, proc_name)
         return
     
-    increment_program(app_name, proc_name)
+    increment_program(cursor, app_name, proc_name)
 
 
-def track():
+def track(cursor: sqlite3.Cursor):
     filename, fullpath = get_active_window_app_name(), get_active_window_app_path()
     name = get_app_name_from_exe(fullpath)
-    write_data(filename, name)
+    write_data(cursor, filename, name)
     return name, filename
 
 
